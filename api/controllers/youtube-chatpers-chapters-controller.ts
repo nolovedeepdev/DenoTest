c06b6b1455ed09dab89e30c24744861e6f749a87
nolovedeepdev <nolovedeepmath@protonmail.com>
2020-06-12 12:50:04 -0300

    stabe api base

--- api/controllers/youtube-chatpers-chapters-controller.ts
+++ api/controllers/youtube-chatpers-chapters-controller.ts
@@ -9,4 +9,109 @@ const getChapters = (
   response.body = chapters
 };
   
-export { getChapters }
+const seachChapter = (id: string, second: string): (IChapter | undefined) =>
+  chapters.filter((item) => item.timeInSeconds === parseInt(second))[0]
+
+
+  // função que chama a seach
+const getChapter = (
+  { params, response }: {
+    params: { id: string; seconds: string };
+    response: any;
+  },
+) => {
+  const chapter: IChapter | undefined = seachChapter(
+    params.id,
+    params.seconds,
+  );
+
+  if (chapter) {
+    response.status = 200;
+    response.body
+  } else {
+    response.status = 404
+    response.body = { message: `Chapter not found` };
+  }
+};
+
+const addChapter = async (
+  { params, request, response }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  },
+) => {
+  const body = await request.body();
+  const newChapter: IChapter = body.value;
+
+  const chapter: IChapter | undefined = seachChapter(
+    params.id,
+    newChapter.timeInSeconds.toString(),
+  );
+
+  if (!chapter) {
+    chapters.push(newChapter)
+    response.status = 201
+    response.body = {message: "OK!"}
+  } else {
+    response.status = 200
+    response.body = {message: "Chapter already exist."}
+  }
+
+}
+
+const updateChapter = async (
+  { params, request, response }: {
+    params: { id: string; seconds: string };
+    request: any;
+    response: any;
+  },
+) => {
+  let chapter: IChapter | undefined = seachChapter(
+    params.id,
+    params.seconds,
+  );
+
+  if (chapter) {
+    const body = await request.body();
+    const updateChapter: { text?: string; time?: string } = body.value;
+
+    chapter = { ...chapter, ...updateChapter }
+
+    for (let idx in chapters) {
+      if (chapters[idx].timeInSeconds === parseInt(params.seconds)) {
+        chapters[idx] = chapter;
+        break;
+      }
+    }
+    response.status = 200;
+    response.body = { message: `OK` };
+  } else {
+    response.status = 404;
+    response.body = { message: `Chapter not found.` };
+  }
+};
+
+const deleteChapter = (
+  { params, response }: {
+    params: { id: string; seconds: string };
+    response: any;
+  },
+) => {
+  let chapter: IChapter | undefined = seachChapter(
+    params.id,
+    params.seconds,
+  );
+  if (chapter) {
+    chapters = chapters.filter((item) =>
+      item.timeInSeconds !== parseInt(params.seconds)
+    );
+    response.status = 200
+    response.body = { message: "OK" }
+  } else {
+    response.status = 404
+    response.body = { message: "Chapter not found." }
+  }
+};
+
+export { getChapters, getChapter, addChapter, updateChapter, deleteChapter };
--- api/routes/routes.ts
+++ api/routes/routes.ts
@@ -1,8 +1,19 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { getChapters } from "../controllers/youtube-chatpers-chapters-controller.ts"
+import {
+    getChapters,
+    getChapter,
+    addChapter,
+    updateChapter,
+    deleteChapter
+} from "../controllers/youtube-chatpers-chapters-controller.ts"
 
 const router = new Router();
 
-router.get("/api/v1/video-chapters/:id", getChapters);
+router.get("/api/v1/video-chapters/:id", getChapters)
+    .get("/api/v1/video-chapters/:id/:seconds", getChapter)
+    .post("/api/v1/video-chapters/:id", addChapter)
+    .put("/api/v1/video-chapters/:id", updateChapter)
+    .delete("/api/v1/video-chapters/:id", deleteChapter);
+
 
 export default router;
